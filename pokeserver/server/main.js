9252f756be00eeb36e632cf17ed4010f73e6ed72
Nelson Gomes <nelson.gomes@brandit.pt>
2018-12-19 16:33:09 +0000

    criação random para inserir poke no mapa e inserção de pasta com icons de pokes

--- pokeserver/server/main.js
+++ pokeserver/server/main.js
@@ -1,5 +1,34 @@
 import { Meteor } from 'meteor/meteor';
+import {Pokemon} from '../imports/collections/pokemon';
+var fs = Npm.require("fs");
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.methods({
+    'pokemon.add': function(loc){
+        var user = this.userId;
+        if(!user){
+          console.log('User not signed in!')
+          return;
+        }
+        var range = 0.035;
+        var range1 = Math.random() > 0.5 ? range: -range;
+        var range2 = Math.random() > 0.5 ? range: -range;
+        var long = loc.longitude;
+
+        long = long + Math.random() * (range1);
+        var lat = loc.latitude;
+        lat = lat + Math.random() * (range2);
+
+        var iconPath = process.env.PWD + '/public';
+        var icons = fs.readdirSync(iconPath);
+
+        var min = Math.ceil(0);
+        var max = Math.ceil(250);
+        var random = Math.floor(Math.random()*(max-min))+min;
+
+        return Pokemon.insert({image: icons[random], longitude: long, latitude: lat})
+    }
 });
